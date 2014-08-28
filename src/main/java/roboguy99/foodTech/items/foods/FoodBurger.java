b317d6bb64e825c1eb4d7c3007cdcab6182800d5
roboguy99 <roboguy99@gmail.com>
2014-08-28 18:46:45 +0100

    Created burger class

---
+++ src/main/java/roboguy99/foodTech/items/foods/FoodBurger.java
@@ -0,0 +1,11 @@
+package roboguy99.foodTech.items.foods;
+
+import net.minecraft.item.ItemFood;
+
+public class FoodBurger extends ItemFood
+{
+	public FoodBurger(int var1, boolean var2)
+	{
+		super(var1, var2);
+	}
+}
