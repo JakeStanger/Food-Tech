897330c2c20785833bcb3199a9854d31f4401381
Roboguy99 <roboguy99@gmail.com>
2014-08-30 11:29:12 +0100

    UE is now working.

--- src/main/java/roboguy99/foodTech/common/tileEntities/TileEntityWindTurbine.java
+++ src/main/java/roboguy99/foodTech/common/tileEntities/TileEntityWindTurbine.java
@@ -8,6 +8,8 @@ import net.minecraft.nbt.NBTTagList;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraftforge.common.util.ForgeDirection;
 import scala.collection.concurrent.INode;
+import universalelectricity.api.UniversalClass;
+import universalelectricity.api.core.grid.INodeProvider;
 
 @UniversalClass
 public class TileEntityWindTurbine extends TileEntity implements ISidedInventory, INodeProvider
