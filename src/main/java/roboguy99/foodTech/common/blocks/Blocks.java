c9a1fec5fe3b1efb30b6de1cd34fa95d486953d8
Roboguy99 <roboguy99@gmail.com>
2014-10-22 20:20:22 +0100

    Removed cable entirely

--- src/main/java/roboguy99/foodTech/common/blocks/Blocks.java
+++ src/main/java/roboguy99/foodTech/common/blocks/Blocks.java
@@ -5,21 +5,19 @@ import net.minecraft.block.material.Material;
 import roboguy99.foodTech.FoodTech;
 import cpw.mods.fml.common.registry.GameRegistry;
 
-public class CreateBlocks //Calls each of the custom block classes, creates any standard new blocks and sets the physical properties of the blocks
+public class Blocks
 {
 	//Create variables for each block
 	public static Block blockWindTurbine;
 	public static Block blockGrindstone;
-	public static Block blockCable;
 	
-	public CreateBlocks()
+	public Blocks()
 	{
 		FoodTech.print("Loading blocks");
 		
 		//Create instances of all of the blocks
 		blockWindTurbine = new BlockWindTurbine(Material.iron);
 		blockGrindstone = new BlockGrindstone(Material.rock);
-		blockCable = new BlockWire(Material.iron);
 		
 		//Call property-setting functions
 		setBlockNames();
@@ -32,7 +30,6 @@ public class CreateBlocks //Calls each of the custom block classes, creates any 
 	{
 		blockWindTurbine.setBlockName("blockWindTurbine");
 		blockGrindstone.setBlockName("blockGrindstone");
-		blockCable.setBlockName("blockCable");
 	}
 	
 	public void setBlockTextureNames() //Loads the texture for each of the blocks
@@ -43,13 +40,11 @@ public class CreateBlocks //Calls each of the custom block classes, creates any 
 	public void setCreativeTabs()
 	{
 		blockGrindstone.setCreativeTab(FoodTech.tabFoodTech);
-		blockCable.setCreativeTab(FoodTech.tabFoodTech);
 	}
 	
 	public void registerBlocks() //Adds the blocks to the game
 	{
 		GameRegistry.registerBlock(blockWindTurbine, "blockWindTurbine");
 		GameRegistry.registerBlock(blockGrindstone, "blockGrindstone");
-		GameRegistry.registerBlock(blockCable, "blockCable");
 	}
 }
