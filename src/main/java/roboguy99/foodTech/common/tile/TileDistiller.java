a124b1186420bb28491907da0ff849fd47262b22
Jake Stanger <roboguy99@gmail.com>
2014-11-10 14:20:52 +0000

    Update TileDistiller.java
    Added temperature-handling code I previously forgot.

--- src/main/java/roboguy99/foodTech/common/tile/TileDistiller.java
+++ src/main/java/roboguy99/foodTech/common/tile/TileDistiller.java
@@ -90,7 +90,8 @@ public class TileDistiller extends Tile implements IInventory
 			shouldMarkDirty = true;
 		}
 		
-		this.temperature--;
+		if(this.isBurning() && this.temperature < 300) this.temperature++;
+		if(!this.isBurning() && this.teperature > 0) this.temperature--;
 		this.processTimeRemaining--;
 		this.timeSpentProcessing++;
 		if(this.furnaceBurnTime > 0) this.furnaceBurnTime--;
