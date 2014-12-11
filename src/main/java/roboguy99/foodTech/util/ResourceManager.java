dd07600adc28fead9f28c94997c360beedcf0dba
Roboguy99 <Roboguy99@gmail.com>
2014-12-11 09:10:32 +0000

    Updated to include new GUI file names

--- src/main/java/roboguy99/foodTech/util/ResourceManager.java
+++ src/main/java/roboguy99/foodTech/util/ResourceManager.java
@@ -14,6 +14,7 @@ public class ResourceManager
 	private static final String MODEL_TEXTURE_PATH = "roboguy99:textures/models/";
 	private static final String BLOCK_TEXTURE_PATH = "roboguy99:textures/blocks";
 	private static final String GUI_TEXTURE_PATH = "roboguy99:textures/gui/";
+	private static final String GUI_ELEMENT_TEXTURE_PATH = "roboguy99:textures/gui/elements";
 	
 	//Textures
 	public static final ResourceLocation GRINDSTONE = new ResourceLocation(ResourceManager.MODEL_TEXTURE_PATH + "grindstone.png");
@@ -29,6 +30,15 @@ public class ResourceManager
 	public static final ResourceLocation TURBINE_TOP = new ResourceLocation(ResourceManager.MODEL_TEXTURE_PATH + "windTurbineTop.png");
 	
 	//GUIs
-	public static final ResourceLocation GRINSTONE_GUI = new ResourceLocation(ResourceManager.GUI_TEXTURE_PATH + "grindstone.png");
+	public static final ResourceLocation BLANK_GUI = new ResourceLocation(ResourceManager.GUI_TEXTURE_PATH + "blank.png");
+	public static final ResourceLocation THREE_SLOT_MACHINE_GUI = new ResourceLocation(ResourceManager.GUI_TEXTURE_PATH + "threeSlotMachine.png");
 	public static final ResourceLocation DISTILLER_GUI = new ResourceLocation(ResourceManager.GUI_TEXTURE_PATH + "distiller.png");
+	
+	//GUI elements
+	public static final ResourceLocation FUEL_PROGRESS_GUI_ELEMENT = new ResourceLocation(ResourceManager.GUI_ELEMENT_TEXTURE_PATH + "fuelProgess.png");
+	public static final ResourceLocation POWER_HORIZONTAL_LARGE_GUI_ELEMENT = new ResourceLocation(ResourceManager.GUI_ELEMENT_TEXTURE_PATH + "powerHorizontalLarge.png");
+	public static final ResourceLocation PROGRESS_SMALL_GUI_ELEMENT = new ResourceLocation(ResourceManager.GUI_ELEMENT_TEXTURE_PATH + "progressSmall.png");
+	public static final ResourceLocation TANK_VERTICAL_GUI_ELEMENT = new ResourceLocation(ResourceManager.GUI_ELEMENT_TEXTURE_PATH + "tankVertical.png");
+	public static final ResourceLocation TANK_VERTICAL_SMALL_GUI_ELEMENT = new ResourceLocation(ResourceManager.GUI_ELEMENT_TEXTURE_PATH + "tankVerticalSmall.png");
+	public static final ResourceLocation TEMPERATURE_HORIZONTAL_GUI_ELEMENT = new ResourceLocation(ResourceManager.GUI_ELEMENT_TEXTURE_PATH + "temperatureHorizontal.png");
 }
